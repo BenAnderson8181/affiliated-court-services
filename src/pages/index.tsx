f9111544cf48df2c1e6504ee2a9cc638ec19ba15
BenAnderson8181 <146896796+BenAnderson8181@users.noreply.github.com>
2023-10-19 18:37:53 -0600

    More work on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
-import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { SignInButton, SignedIn, SignedOut } from "@clerk/nextjs";
 import type { NextPage } from "next";
 import Image from "next/image";
 import Head from "next/head";
 import Link from "next/link";
 import HomeButton from "~/components/MainHomeButton";
+import { RiGovernmentFill, RiCreativeCommonsByFill, RiStore3Fill, RiHeartPulseFill, RiCapsuleFill } from "react-icons/ri";
 
 import logo from "~/../public/400JpgdpiLogoCropped.jpg";
+import main from "~/../public/water-home-unsplash.jpg";
 
 const Home: NextPage = () => {
   return (
@@ -15,10 +17,10 @@ const Home: NextPage = () => {
         <meta name="description" content="Affiliated Court Services Management" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-slate-100">
-        <section className="h-20 fixed top-0 w-full z-50 text-slate-100 bg-gradient-to-b from-[#2f0f5b] via-[#6941a2] to-[#2f0f5b]">
+      <main className="flex flex-col h-screen">
+        <section className="w-full h-32 z-50 text-slate-100 bg-gradient-to-b from-[#2f0f5b] via-[#6941a2] to-[#2f0f5b]">
           <div className="flex justify-between px-10">
-            <div className="">
+            <div className="inline-flex">
               <div className="bg-slate-100 rounded mt-1.5 p-1">
                 <Image 
                   src={logo}
@@ -26,11 +28,34 @@ const Home: NextPage = () => {
                   width={90}
                 />
               </div>
+              <p className="font-bold font-serif text-2xl mt-7 ml-3 [text-shadow:_0_1px_0_rgb(0_0_0_/_80%)]">AFFILIATED COURT SERVICES</p>
             </div>
-            <div className="bg-transparent font-bold font-serif text-4xl mt-5">AFFILIATED COURT SERVICES</div>
             <div className="pt-7">
+              <Link href="/services" className="border border-purple-950 text-slate-100 bg-amber-400 p-2 rounded-xl text-xl font-extrabold shadow-lg shadow-black hover:shadow-md hover:shadow-black hover:scale-105 [text-shadow:_0_1px_0_rgb(0_0_0_/_80%)]" passHref>See our client services</Link>
+            </div>
+          </div>
+        </section>
+        <section className="fixed w-32 bg-violet-100 left-0 h-full text-purple-950 top-20 pt-10 text-xl shadow-2xl shadow-purple-950 z-20 font-bold">
+          <div className="pb-3 flex justify-center px-3 border-b border-purple-950"><HomeButton /></div>
+          <div className="pt-3 px-3"><Link href='/about-us' passHref>About Us</Link></div>
+          <div className="px-3"><Link href='/services' passHref>Services</Link></div>
+          <div className="px-3"><Link href='/resources' passHref>Resoures</Link></div>
+          <div className="px-3"><Link href='/signInRedirect' passHref>Account</Link></div>
+          <div className="px-3"><Link href='/calendar' passHref>Calendar</Link></div>
+          <div className="px-3"><Link href='/account/search' passHref>Search</Link></div>
+        </section>
+        <section className="h-full w-full flex flex-col relative overflow-hidden">
+          <div>
+            <p className="absolute left-1/4 top-16 text-4xl font-serif font-light text-purple-900 [text-shadow:_0_1px_0_rgb(0_0_0_/_40%)]">Welcome to Affiliated Court Services</p>
+            <p className="absolute left-1/4 top-28 text-3xl font-serif font-thin translate-x-6 text-purple-950 [text-shadow:_0_1px_0_rgb(0_0_0_/_40%)]">Your Journey to Well Being Starts Here</p>
+            <Image
+              src={main}
+              alt="water"
+              className="w-full object-contain min-h-0"
+            /> 
+            <div className="absolute left-1/2 bottom-16 z-50">
               <SignedIn>
-                <div className="border-2 border-purple-950 p-.5 rounded-full shadow-md shadow-black"><UserButton /></div>
+                <Link href='/signInRedirect' className="border-2 border-purple-950 text-purple-950 bg-transparent p-8 rounded-xl text-xl font-extrabold shadow-lg shadow-purple-950 hover:shadow-md hover:shadow-black hover:bg-purple-950 hover:text-slate-100 [text-shadow:_0_1px_0_rgb(0_0_0_/_30%)]" passHref>Take me to my account!</Link>
               </SignedIn>
               <SignedOut>
                 <SignInButton mode='modal'>
@@ -42,20 +67,22 @@ const Home: NextPage = () => {
             </div>
           </div>
         </section>
-        <section className="fixed w-32 bg-violet-100 left-0 h-full text-purple-950 top-20 pt-5 text-xl shadow-2xl shadow-purple-950 z-20 font-bold">
-          <div className="pb-3 flex justify-center px-3 border-b border-purple-950"><HomeButton /></div>
-          <div className="pt-3 px-3"><Link href='/about-us' passHref>About Us</Link></div>
-          <div className="px-3"><Link href='/services' passHref>Services</Link></div>
-          <div className="px-3"><Link href='/resources' passHref>Resoures</Link></div>
-          <div className="px-3"><Link href='/signInRedirect' passHref>Account</Link></div>
-          <div className="px-3"><Link href='/calendar' passHref>Calendar</Link></div>
-          <div className="px-3"><Link href='/account/search' passHref>Search</Link></div>
-        </section>
-        <section className="text-slate-900">
-          Main
-        </section>
-        <section className="fixed bg-purple-900 bottom-0 h-36 w-full left-32 text-slate-100">
-          Bottom
+        <section className="pl-32 bg-gradient-to-b from-purple-950 to-purple-500 w-full text-slate-100 shadow-inner shadow-slate-800">
+          <div className="flex justify-evenly p-10 text-slate-100 text-lg">
+            <div className="-translate-x-16">
+              <div><RiGovernmentFill size='1.8rem' className="text-slate-100 inline mr-3 -translate-y-1" />Comprehensive Court-Approved Evaluations</div>
+              <div><RiCapsuleFill size='1.8rem' className="text-slate-100 inline mr-3 -translate-y-1" />Substance Use Disorder Group Therapy Sessions</div>
+              <div><RiHeartPulseFill size='1.8rem' className="text-slate-100 inline mr-3 -translate-y-1" />Domestic Violence Group Therapy Sessions</div>
+              <div><RiCreativeCommonsByFill size='1.8rem' className="text-slate-100 inline mr-3 -translate-y-1" />Individual Therapy Sessions</div>
+              <div><RiStore3Fill size='1.8rem' className="text-slate-100 inline mr-3 -translate-y-1" />Diversion Workshops</div>
+            </div>
+            <div className="translate-x-8">
+              <p>Founded by <span className="font-serif">Lujean Marshall</span></p>
+              <p>Over 30 years of experience</p>
+              <p>801-888-8888</p>
+              <p>info@affiliatedcourtservices.org</p>
+            </div>
+          </div>
         </section>
       </main>
     </>
